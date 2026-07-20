044ddacd27adc45033b5065aaf062b2df3098417
변준성 <bjszz71344@gmail.com>
2026-07-20 09:02:13 +0900

    feat(server): Spring Boot 기본 설정 및 프로젝트 초기화 [SRV-001]

---
+++ Server/IoTServer/src/main/java/com/example/iotserver/IoTServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTServerApplication.class, args);
+    }
+
+}
